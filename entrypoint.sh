2816a26be93d3acf975e66d6746ab8296f28e776
joeymos3r <w9dje8kexodm@atomicmail.io>
2026-08-13 01:22:02 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -e
 
+# تنظیم پورت
 if [ -n "$PORT" ]; then
   sed -i "s/listen 3000;/listen ${PORT};/g" /etc/nginx/http.d/default.conf
 fi
 
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+# اجرای x-ui در پس‌زمینه
+x-ui start &
+
+# صبر کن x-ui بالا بیاد
+sleep 2
+
+# اجرای nginx در foreground
+exec nginx -g "daemon off;"
