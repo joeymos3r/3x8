29dd2a7a4424d992a2c7ccd4926a28558257efe6
joeymos3r <w9dje8kexodm@atomicmail.io>
2026-08-12 23:27:14 +0330

    Modify entrypoint.sh to start nginx and run x-ui
    Updated entrypoint script to start nginx in the background and replace the current process with x-ui.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-x-ui start &
+# Start nginx as daemon (background)
+nginx -g "daemon on;"
 
-sleep 5
-
-nginx -g 'daemon off;'
+# Replace current process with x-ui as PID 1
+exec x-ui run
