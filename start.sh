17e9204b13787e9b815f09cac798c71b77f8b465
joeymos3r <w9dje8kexodm@atomicmail.io>
2026-08-13 00:20:59 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 cd /usr/local/x-ui
-./x-ui start
 
+# تنظیم پورت پنل
+./x-ui setting -port 2053
+
+# اجرای پنل در پس‌زمینه
+./x-ui start &
+
+# صبر تا پنل بالا بیاد
+sleep 5
+
+# اجرای Nginx در foreground
 nginx -g 'daemon off;'
