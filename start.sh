e94abec045df178b9999c6c9b38f5b89797ee69a
joeymos3r <w9dje8kexodm@atomicmail.io>
2026-08-13 00:08:14 +0330

    Clean up start.sh by removing comments
    Removed comments and redundant command for nginx configuration.

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Start 3x-ui panel v3.6.0
 cd /usr/local/x-ui
 ./x-ui start
 
-# Replace port variable in nginx template and start nginx
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
